152a9b6a89dd6dfa26be009cef9a1a411545eab4
Dave Perrett <dave@recurser.com>
2015-12-02 20:10:31 +0900

    Add a route to create a basic word version of an idea.

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,16 @@
+class IdeasController < ApplicationController
+  respond_to :docx
+
+  before_action :find_idea
+
+  def show
+    authorize(@idea)
+    respond_with(@idea, filename: "idea-#{@idea.to_param}.docx", word_template: 'show.docx')
+  end
+
+  private
+    def find_idea
+      @idea = Idea.find(params[:id])
+    end
+
+end
