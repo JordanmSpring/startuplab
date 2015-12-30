c7ce1c0a2ccc14329303e858adab9feff01bed72
Dave Perrett <dave@recurser.com>
2015-12-30 19:09:27 +0900

    Add correct body text to the notification email.

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -2,4 +2,8 @@ module PathsHelper
   def angular_idea_path(idea)
     "/#/ideas/#{idea.id}"
   end
+
+  def angular_idea_url(idea)
+    "http://#{App.mailer_default_url_options[:host]}#{angular_idea_path(idea)}"
+  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,12 @@
 class UserMailer < ApplicationMailer
+  helper :paths
 
   # Sends an invitation to subscribe to alert emails.
   def idea_notification_email(idea)
-    @idea    = idea
-    @to      = @idea.user.email
-    @subject = 'Idea notification'
+    @idea     = idea
+    @comments = idea.comments.approved.not_notified
+    @to       = idea.user.email
+    @subject  = I18n.t('user_mailer.idea_notification_email.subject', name: idea.name, count: @comments.count)
     mail(to: @to, subject: @subject)
   end
 
