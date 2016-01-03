4a42d54cb89c64887f39169694c998490cb743c1
Dave Perrett <dave@recurser.com>
2016-01-03 17:36:31 +0900

    Add OmniAuth fields to the User table.

---
+++ db/migrate/20160103083522_add_omniauth_to_users.rb
@@ -0,0 +1,10 @@
+class AddOmniauthToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+
+    add_index :users, :provider
+    add_index :users, :uid
+    add_index :users, [:provider, :uid]
+  end
+end
