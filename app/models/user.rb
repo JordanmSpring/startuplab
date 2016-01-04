b7c2600977d566c854dfbada1e4ca7676972c88f
Dave Perrett <dave@recurser.com>
2016-01-04 18:59:46 +0900

    Store social provider and social ID for existing users who later log in with omniauth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,11 +59,18 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-    User.find_by(email: auth.info.email) || where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      # Twitter doesn't provide email addresses.
-      user.email    = (auth.provider == 'twitter' ? '' : auth.info.email)
-      user.password = Devise.friendly_token[0,20]
-      user.name     = auth.info.name
+    existing = User.find_by(email: auth.info.email)
+    if existing
+        existing.tap do |user|
+          user.update_attributes(provider: auth.provider, uid: auth.uid)
+        end
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        # Twitter doesn't provide email addresses.
+        user.email    = (auth.provider == 'twitter' ? '' : auth.info.email)
+        user.password = Devise.friendly_token[0,20]
+        user.name     = auth.info.name
+      end
     end
   end
 
