be5d9f2a15088b05a7d16042eb1b98ab0ea1227c
Dave Perrett <dave@recurser.com>
2016-02-06 18:16:11 +0900

    Show the status of the invitation in the share list.

--- app/exhibits/user_idea_exhibit.rb
+++ app/exhibits/user_idea_exhibit.rb
@@ -7,9 +7,20 @@ class UserIdeaExhibit
 
   def as_json(options = {})
     {
-      id:   object.id,
-      user: UserExhibit.new(object.shared_user),
-      idea: IdeaExhibit.new(object.shared_idea, User.new),
+      id:     object.id,
+      status: status,
+      user:   UserExhibit.new(object.shared_user),
+      idea:   IdeaExhibit.new(object.shared_idea, User.new),
     }.as_json
   end
+
+  private
+
+    def status
+      if object.shared_user.sign_in_count == 0
+        'pending'
+      else
+        'accepted'
+      end
+    end
 end
