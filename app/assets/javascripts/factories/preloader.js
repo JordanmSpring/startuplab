cb41c73758eb9ccb6ea5c0ed7e23872924cc85c1
Andrew King <me@andrewsking.com>
2016-04-16 17:02:00 +1000

    Add preloader model

---
+++ app/assets/javascripts/factories/preloader.js
@@ -0,0 +1,26 @@
+angular.module('StartupLab')
+.factory('Preloader', [ function() {
+
+  function Preloader(imageLocations) {
+    this.imageLocations = imageLocations;
+  }
+
+  Preloader.preloadImages = function(imageLocations){
+    var preloader = new Preloader(imageLocations);
+    preloader.load();
+  };
+
+  Preloader.prototype = {
+    load: function(){
+      for(var i = 0; i < this.imageLocations.length; i++){
+        this.loadImageLocation(this.imageLocations[i]);
+      }
+    },
+    loadImageLocation: function(imageLocation){
+      $(new Image()).prop('src', imageLocation);
+    }
+  };
+
+  return Preloader;
+
+}]);
